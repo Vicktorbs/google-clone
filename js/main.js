13ebdd1be65d9207dcb5eac733800331ab83bdbb
Víctor Bolaños Solís <viktorb.132@gmail.com>
2022-03-06 00:18:28 -0600

    Desktop prportion

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const form = document.getElementById('google-form')
 const body = document.getElementById('main-container')
+const party = document.getElementById('party-section')
 
 function getIputValue(e) {
     e.preventDefault();
     let searchValue = document.getElementById('search')
     if (searchValue.value === 'fer') {
         console.log('Happy Birthday');
+        party.style.display = 'block'
         body.classList.add('party')
     } else {
         form.classList.add('bad-input')
