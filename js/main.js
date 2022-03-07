1d62c763ccbbd9c7823dc48520643527a22522f4
Víctor Bolaños Solís <viktorb.132@gmail.com>
2022-03-07 00:46:16 -0600

    imagenes, hoe clicking

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 const form = document.getElementById('google-form')
 const body = document.getElementById('main-container')
 const party = document.getElementById('party-section')
+const footer = document.getElementById('footer')
+const message = document.getElementsByClassName('text-hoe')
+const imgHoe = document.getElementById('img-hoe')
 
 function getIputValue(e) {
     e.preventDefault();
+    console.log(message);
     let searchValue = document.getElementById('search')
     if (searchValue.value === 'fer') {
         console.log('Happy Birthday');
         party.style.display = 'block'
         body.classList.add('party')
+        footer.style.display = 'none'
     } else {
         form.classList.add('bad-input')
         searchValue.value = ''
@@ -16,4 +21,16 @@ function getIputValue(e) {
     }
 }
 
+imgHoe.addEventListener('click', () => {
+    if (message[1].className.includes('active')) {
+        console.log('in');
+        for (let i = 0; i < message.length; i++) {
+            message[i].classList.remove('active')
+        }
+    } else {
+        for (let i = 0; i < message.length; i++) {
+            message[i].classList.add('active')
+        }
+    }
+})
 form.addEventListener('submit', getIputValue);
